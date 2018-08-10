b9ac9160398c4d04ce16d49239d434f63621e797
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-10 21:30:48 +0100

    REF: Add show_log_and_files function

--- git_practice/scripts/demonstrate_rebase.sh
+++ git_practice/scripts/demonstrate_rebase.sh
@@ -5,8 +5,14 @@ SEPARATOR_0="==========================="
 SEPARATOR_1="=============="
 SEPARATOR_2="=============================="
 
+show_log_and_files() {
+    git log --oneline
+    echo -e "\nAnd its files:"
+    ls -lh
+}
+
 press_any_key() {
-    read -rsp $'Press any key to continue...\n' -n1 key
+    read -rsp $'Press any key to continue...\n\n' -n1 key
 }
 
 cd $REPO_NAME
@@ -18,18 +24,12 @@ echo -e "$SEPARATOR_0\n"
 
 git checkout master
 echo "The master branch log before rebasing:"
-git log --oneline
-echo -e "\nAnd its files:"
-ls -lh
+show_log_and_files
 
 git checkout feature-branch
 echo "The feature-branch log before rebasing:"
-git log --oneline
-echo -e "\nAnd its files:"
-ls -lh
+show_log_and_files
 press_any_key
-echo -e "\n"
-
 
 echo $SEPARATOR_1
 echo "PERFORM REBASE"
@@ -37,23 +37,18 @@ echo -e "$SEPARATOR_1\n"
 
 git rebase master
 press_any_key
-echo -e "\n"
 
 # See git logs and files on master and feature-branch after rebase
 echo "The feature-branch log after rebasing:"
-git log --oneline
-echo -e "\nAnd its files:"
-ls -lh
+show_log_and_files
 echo -e "\n"
 
 git checkout master
 
 echo "The master branch log after rebasing, but before merging:"
-git log --oneline
-echo -e "\nAnd its files:"
-ls -lh
+show_log_and_files
+
 press_any_key
-echo -e "\n"
 
 echo $SEPARATOR_2
 echo "PERFORM (FAST-FORWARD) MERGE"
@@ -61,11 +56,8 @@ echo -e "$SEPARATOR_2\n"
 
 git merge feature-branch
 press_any_key
-echo -e "\n"
 
 # Preview git logs and files on master after rebase and merge
 echo "The master branch log after rebasing and merging:"
-git log --oneline
-echo -e "\nAnd its files:"
-ls -lh
+show_log_and_files
 echo -e "\n"
